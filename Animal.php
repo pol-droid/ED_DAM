8aebc2eb02535a48f9fe4948652818131dd9d1e8
pol-droid <pol.mestre@insbaixcamp.cat>
2025-03-28 19:05:09 +0100

    Nuevo fichero PHP Classe Animal

---
+++ Animal.php
@@ -0,0 +1,20 @@
+<?php
+
+class Animal {
+
+    private $nom;
+    private $edat;
+
+    public function __construct($nomP, $edatP){
+
+        $this -> nom = $nomP;
+        $this -> edat = $edatP;
+
+        }
+    
+        
+    }
+
+
+
+?>
